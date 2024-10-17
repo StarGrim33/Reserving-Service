034a1fe06db67a7bd5284592462b6f376c698a59
twilight <twilight@deeplay.io>
2024-10-17 19:30:21 +0300

    Infrastructure layer finish
    Infrastructure layer finish

---
+++ Bookify/src/Bookify.Infrastructure/Configurations/BookingConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Shared;
+using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.AmenitiesUpCharge, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.Duration);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(booking => booking.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(booking => booking.UserId);
+    }
+}
---
+++ Bookify/src/Bookify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Bookify.Application.Abstractions.Data;
+using Npgsql;
+
+namespace Bookify.Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory(string connectionString) : ISqlConnectionFactory
+{
+    private readonly string _connectionString = connectionString;
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+        return connection;
+    }
+}
